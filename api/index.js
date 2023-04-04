9c10231aecc59c21abff0213813dda2e6cac6985
kanmus7 <sebastiangc.2309@gmail.com>
2023-04-03 23:33:53 -0500

    deploy

--- api/index.js
+++ api/index.js
@@ -4,20 +4,25 @@ const routerApi = require('./routes')
 const {logErrors, errorHandler, boomErrorHandler} = require('./middlewares/error.handler')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 app.use(express.json()) //middleware para recibir informacion de tipo json
 
-const whitelist = ['http://localhost:8080', 'http://localhost:3001']
+const whitelist = ['http://localhost:8080']
 const options = {
   origin: (origin, callback) => {
     if(whitelist.includes(origin)){
       callback(null, true)
     } else {
-      callback(new Error('no permitido', false))
+      callback(new Error('no permitido'))
     }
   }
 }
-app.use(cors(options))
+app.use(cors())
+
+app.get('/api', (req,res) => {
+  res.send('express server')
+})
+
 routerApi(app)
 /* los middlewares deben ir despues del routing */
 app.use(logErrors)
